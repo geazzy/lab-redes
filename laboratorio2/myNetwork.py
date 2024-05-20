54a63c4a3bb54b88f9acd8abcadf215b3bd111a9
Geazzy B Marçal Zanoni <zanonigb@gmail.com>
2024-05-20 20:28:27 -0300

    Rede completa  para o laboratorio2

--- laboratorio2/myNetwork.py
+++ laboratorio2/myNetwork.py
@@ -12,14 +12,12 @@ def setupNetwork():
 
     # Add NAT router and configure IP forwarding
     info('*** Adding NAT router and enabling IP forwarding\n')
-    r1 = net.addNAT('r1', connect=None, ip='10.100.0.1/30')
+    r1 = net.addHost('r1', ip='10.100.0.1/30')  # Corrigido para addHost
     r1.cmd('sysctl net.ipv4.ip_forward=1')
-    
 
     # Add second router and configure IP forwarding
     r2 = net.addHost('r2', ip='10.100.0.2/30')
     r2.cmd('sysctl net.ipv4.ip_forward=1')
-   
 
     # Add switches
     info('*** Adding switches\n')
@@ -29,15 +27,15 @@ def setupNetwork():
     # Add links between routers and switches
     info('*** Adding links\n')
     net.addLink(r1, r2, intfName1='r1-eth0', intfName2='r2-eth0', params1={'ip': '10.100.0.1/30'}, params2={'ip': '10.100.0.2/30'})
-    net.addLink(s1, r2, intfName2='r2-eth1', params2={'ip': '192.168.1.1/26'})
-    net.addLink(s2, r2, intfName2='r2-eth2', params2={'ip': '192.168.2.1/23'})
+    net.addLink(s1, r2, intfName2='r2-eth1', params2={'ip': '10.100.1.1/26'})
+    net.addLink(s2, r2, intfName2='r2-eth2', params2={'ip': '10.100.2.1/23'})
 
     # Add hosts
     info('*** Adding hosts\n')
-    h1 = net.addHost('h1', ip='192.168.1.2/26', defaultRoute='via 192.168.1.1')
-    h2 = net.addHost('h2', ip='192.168.1.3/26', defaultRoute='via 192.168.1.1')
-    h3 = net.addHost('h3', ip='192.168.2.2/23', defaultRoute='via 192.168.2.1')
-    h4 = net.addHost('h4', ip='192.168.2.3/23', defaultRoute='via 192.168.2.1')
+    h1 = net.addHost('h1', ip='10.100.1.2/26', defaultRoute='via 10.100.1.1')
+    h2 = net.addHost('h2', ip='10.100.1.3/26', defaultRoute='via 10.100.1.1')
+    h3 = net.addHost('h3', ip='10.100.2.2/23', defaultRoute='via 10.100.2.1')
+    h4 = net.addHost('h4', ip='10.100.2.3/23', defaultRoute='via 10.100.2.1')
 
     # Connect hosts to switches
     info('*** Connecting hosts to switches\n')
@@ -46,18 +44,19 @@ def setupNetwork():
     net.addLink(h3, s2)
     net.addLink(h4, s2)
 
-  
     # Start the network
     info('*** Starting network\n')
     net.start()
-    
-    # Configura NAT no roteador R1 para acesso à Internet
+
+    # Configurar NAT e rotas nos roteadores
+    info('*** Configurando NAT e rotas\n')
     r1.cmd('iptables -t nat -A POSTROUTING -o enp0s1 -j MASQUERADE')
-     # Configurar default route em R2 para R1
+    r1.cmd('ip route add 10.100.1.0/26 via 10.100.0.2 dev r1-eth0')
+    r1.cmd('ip route add 10.100.2.0/23 via 10.100.0.2 dev r1-eth0')
+
     r2.cmd('ip route add default via 10.100.0.1 dev r2-eth0')
     r2.cmd('iptables -t nat -A POSTROUTING -o r2-eth0 -j MASQUERADE')
-    
-    
+
     # Run the CLI
     info('*** Running CLI\n')
     CLI(net)
